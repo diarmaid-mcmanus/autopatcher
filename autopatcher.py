595f1b87f4d72ba01020e1175a48241f1a290acd
diarmaid-mcmanus <@MadcapOcelot>
2017-11-22 22:25:29 +0000

    print statically exported attack surface

--- autopatcher.py
+++ autopatcher.py
@@ -2,6 +2,13 @@ import subprocess
 import xml.etree.ElementTree as etree
 from subprocess import PIPE as SPIPE
 
+def is_exported(element):
+    try:
+        if element.get('{http://schemas.android.com/apk/res/android}exported') == 'true':
+            return True
+    except TypeError:
+        return False
+
 apk_file = "example.apk"
 debuggable_attribute = "{http://schemas.android.com/apk/res/android}debuggable"
 apktool_decode = ["apktool", "d", apk_file ]
@@ -9,8 +16,10 @@ apktool_build = ["apktool", "b", "example", "-oexample-debug.apk"]
 jarsigner_sign = ["jarsigner", "-verbose", "-sigalg", "SHA1withRSA", 
     "-digestalg", "SHA1", "-keystore", "autopatcher.jks", "-storepass", "password",
     "example-debug.apk", "autopatcher-keystore" ]
+attack_surface = [ 'activity', 'service', 'receiver', 'provider' ]
 
 # TODO verify properly signed apk
+# Shouldn't I be able to do this with Androguard?
 
 
 subprocess.run(apktool_decode, stdout=SPIPE, stderr=SPIPE)
@@ -20,6 +29,13 @@ manifest_file = etree.parse('example/AndroidManifest.xml')
 for child in manifest_file.getroot():
     if child.tag == "application":
         child.attrib[debuggable_attribute] = 'true'
+        for application_child in child.iter():
+            if application_child.tag in attack_surface:
+                if(is_exported(application_child)):
+                    # yup
+                    print(etree.tostring(application_child, encoding='unicode'))
+
+# This is where I should do initial static (Manifest) analysis
 
 manifest_file.write('example/AndroidManifest.xml', encoding="unicode")
 
